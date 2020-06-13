c693e77e1c1b247f68167bbc06169fe2e1856edc
AntonyVial <48533315+AntonyVial@users.noreply.github.com>
2020-06-13 23:32:09 +0200

    Create WEB_PROJET.js

---
+++ WEB_PROJET.js
@@ -0,0 +1,7 @@
+function resetFields()
+{
+	document.getElementById('Pseudo').value="";
+	document.getElementById('commentaire').value="";
+	document.getElementById('Note').value="";
+	document.getElementById('select1').selectedIndex=-1;
+}
